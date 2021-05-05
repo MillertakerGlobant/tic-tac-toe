0f1989583d4c0974ed87155dc368805909a44fc0
MillertakerGlobant <gb_mgonzalez@indeed.com>
2021-05-05 10:50:01 -0500

    Board test pass

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,6 +3,7 @@ import BoardItem from './BoardItem';
 
 const Board = () => {
   const [board, setBoard] = useState([-1,-1,-1,-1,-1,-1,-1,-1,-1]);
+  const [turn, setTurn] = useState(0);
 
   useEffect(() => {
     console.log('on changed')
@@ -12,10 +13,10 @@ const Board = () => {
     var index = e.target.getAttribute('arrayindex');
     var currentBoard = JSON.parse(JSON.stringify(board));
 
-    currentBoard[index] = index % 2;
+    currentBoard[index] = turn % 2;
+    setTurn(turn + 1);
 
     setBoard(currentBoard); 
-    console.log(board);
   }
 
   const boardItems = board.map((e, i) => <BoardItem key={`boardItem${i}`} arrayindex={i} value={e} onClick={clickItem} ></BoardItem> )
